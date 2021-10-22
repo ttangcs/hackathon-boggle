0644de838d40c5ed907731f25a079785f3f24a35
ttangcs <ttangcs@gmail.com>
2021-10-22 11:51:12 -0600

    partially working

--- main.py
+++ main.py
@@ -8,25 +8,24 @@ import string
 import time
 
 
-def getChildNode(currentNode, string):
-  if len(string) > 1:
-    try:
-      return getChildNode(currentNode.childNodes[[ node.char for node in currentNode.childNodes ].index(string[:1])], string[1:])
-    except:
-      return currentNode
-
-  try:
-    return currentNode.childNodes[[ node.char for node in currentNode.childNodes ].index(string)]
-  except:
-    return currentNode
-    
-
 class TrieNode:
   def __init__(self, value = "", isRoot=False):
       self.childNodes = []
       self.char = value
       self.isRoot = isRoot
       self.isEndOfWord = False
+
+  def getChildNode(self, string):
+    if len(string) > 1:
+      try:
+        return self.getChildNode(self.childNodes[[ node.char for node in self.childNodes ].index(string[:1])], string[1:])
+      except:
+        return self
+
+    try:
+      return self.childNodes[[ node.char for node in self.childNodes ].index(string)]
+    except:
+      return self
 
 class Trie:
   def __init__(self):
@@ -40,7 +39,7 @@ class Trie:
 
   def insertWord(self, word):
     currentNode = self.root
-    for char in list(word):
+    for char in list(word.lower()):
       if not self._contains(currentNode.childNodes, char):
         currentNode.childNodes.append(TrieNode(char))
       currentNode = currentNode.childNodes[[ node.char for node in currentNode.childNodes].index(char)]
@@ -54,15 +53,10 @@ file = open("./words.txt", "r")
 wordLines = file.read().splitlines()
 allowedChars = set(string.ascii_lowercase)
 
-count = 0
-
 for word in wordLines:
   if len(word) >= 3 and set(word) <= allowedChars:
-    count += 1
-    # print (word)
     trie.insertWord(word)
-  # if count == 20:
-  #   break
+
 
 
 driver = webdriver.Chrome()
@@ -71,6 +65,7 @@ driver.get("http://flooreight.com/boggle/")
 # Start the game
 startElement = driver.find_element(By.XPATH, "//button[text()='Start']")
 inputElement = driver.find_element(By.TAG_NAME, "input")
+enterElement = driver.find_element(By.XPATH, "//button[text()='Ok']")
 
 grid = []
 gridRowElements = driver.find_elements(By.CLASS_NAME, "boggle-board__row")
@@ -103,37 +98,53 @@ def searchWord(currentNode, rowIdx, columnIdx, usedGrid, wordString, words):
     words.add(wordString)
 
   # usedGrid[rowIdx][columnIdx] = True # Set the letter visited
-  # if inBoundsAndNotVisited(rowIdx - 1, columnIdx - 1, usedGrid): # Up and Left
-  #   character = grid[rowIdx - 1][columnIdx - 1]
-  #   words.update(searchWord(getChildNode(currentNode, character), rowIdx - 1, columnIdx - 1, usedGrid, wordString + character, words))
+  if inBoundsAndNotVisited(rowIdx - 1, columnIdx - 1, usedGrid): # Up and Left
+    character = grid[rowIdx - 1][columnIdx - 1]
+    childNode = currentNode.getChildNode(character)
+    if childNode != currentNode:
+      words.update(searchWord(childNode, rowIdx - 1, columnIdx - 1, usedGrid, wordString + character, words))
 
-  # if inBoundsAndNotVisited(rowIdx - 1, columnIdx, usedGrid): # Up
-  #   character = grid[rowIdx - 1][columnIdx]
-  #   words.update(searchWord(getChildNode(currentNode, character), rowIdx - 1, columnIdx, usedGrid, wordString + character, words))
+  if inBoundsAndNotVisited(rowIdx - 1, columnIdx, usedGrid): # Up
+    character = grid[rowIdx - 1][columnIdx]
+    childNode = currentNode.getChildNode(character)
+    if childNode != currentNode:
+      words.update(searchWord(childNode, rowIdx - 1, columnIdx, usedGrid, wordString + character, words))
 
   if inBoundsAndNotVisited(rowIdx - 1, columnIdx + 1, usedGrid): # Up and Right
     character = grid[rowIdx - 1][columnIdx + 1]
-    words.update(searchWord(getChildNode(currentNode, character), rowIdx - 1, columnIdx + 1, usedGrid, wordString + character, words))
+    childNode = currentNode.getChildNode(character)
+    if childNode != currentNode:
+      words.update(searchWord(childNode, rowIdx - 1, columnIdx + 1, usedGrid, wordString + character, words))
 
-  # if inBoundsAndNotVisited(rowIdx, columnIdx - 1, usedGrid): # Left
-  #   character = grid[rowIdx][columnIdx - 1]
-  #   words.update(searchWord(getChildNode(currentNode, character), rowIdx, columnIdx - 1, usedGrid, wordString + character, words))
+  if inBoundsAndNotVisited(rowIdx, columnIdx - 1, usedGrid): # Left
+    character = grid[rowIdx][columnIdx - 1]
+    childNode = currentNode.getChildNode(character)
+    if childNode != currentNode:
+      words.update(searchWord(childNode, rowIdx, columnIdx - 1, usedGrid, wordString + character, words))
 
   if inBoundsAndNotVisited(rowIdx, columnIdx + 1, usedGrid): # Right
     character = grid[rowIdx][columnIdx + 1]
-    words.update(searchWord(getChildNode(currentNode, character), rowIdx, columnIdx + 1, usedGrid, wordString + character, words))
+    childNode = currentNode.getChildNode(character)
+    if childNode != currentNode:
+      words.update(searchWord(childNode, rowIdx, columnIdx + 1, usedGrid, wordString + character, words))
 
-  # if inBoundsAndNotVisited(rowIdx + 1, columnIdx - 1, usedGrid): # Down and Left
-  #   character = grid[rowIdx + 1][columnIdx - 1]
-  #   words.update(searchWord(getChildNode(currentNode, character), rowIdx + 1, columnIdx - 1, usedGrid, wordString + character, words))
+  if inBoundsAndNotVisited(rowIdx + 1, columnIdx - 1, usedGrid): # Down and Left
+    character = grid[rowIdx + 1][columnIdx - 1]
+    childNode = currentNode.getChildNode(character)
+    if childNode != currentNode:
+      words.update(searchWord(childNode, rowIdx + 1, columnIdx - 1, usedGrid, wordString + character, words))
 
   if inBoundsAndNotVisited(rowIdx + 1, columnIdx, usedGrid): # Down
     character = grid[rowIdx + 1][columnIdx]
-    words.update(searchWord(getChildNode(currentNode, character), rowIdx + 1, columnIdx, usedGrid, wordString + character, words))
+    childNode = currentNode.getChildNode(character)
+    if childNode != currentNode:
+      words.update(searchWord(childNode, rowIdx + 1, columnIdx, usedGrid, wordString + character, words))
 
   if inBoundsAndNotVisited(rowIdx + 1, columnIdx + 1, usedGrid): # Down and Right
     character = grid[rowIdx + 1][columnIdx + 1]
-    words.update(searchWord(getChildNode(currentNode, character), rowIdx + 1, columnIdx + 1, usedGrid, wordString + character, words))
+    childNode = currentNode.getChildNode(character)
+    if childNode != currentNode:
+      words.update(searchWord(childNode, rowIdx + 1, columnIdx + 1, usedGrid, wordString + character, words))
   return words
   
 # Get the grid
@@ -148,21 +159,19 @@ for rowIndex in range(maxRows):
   for columnIndex in range(maxColumns):
     wordString = ""
     usedGrid = [[False] * (maxColumns)] * (maxRows)
-    searchWord(trie.root, rowIndex, columnIndex, usedGrid, wordString + grid[rowIndex][columnIndex], words)
+    searchWord(trie.root.getChildNode(grid[rowIndex][columnIndex]), rowIndex, columnIndex, usedGrid, wordString + grid[rowIndex][columnIndex], words)
 
-
-def printTrie(node, string):
-  string = string + " * " + node.char + " * "
-  for childNode in node.childNodes:
-    string = string + childNode.char
-    string = printTrie(childNode, string)
-  string = string + " - "
-  return string
 
 printString = ""
-# print(printTrie(trie.root, printString))
 print(words)
 
 print(grid)
 
-inputElement.send_keys("Hi", Keys.ENTER),
+
+for word in words:
+  inputElement.send_keys(word)
+  enterElement.click()
+  if inputElement.get_attribute("class") == "invalid":
+    inputElement.clear()
+
+while (EC.)
